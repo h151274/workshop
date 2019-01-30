817555a08cc003a74c0524c3e6963d054c6b58d1
h151274 <bonn_96aa@hotmail.com>
2019-01-30 19:01:01 +0100

    Updates

---
+++ precache-manifest.b656c100938b6820df2745d67c298620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "69e3d247204b19396307",
+    "url": "/workshop/static/js/main.69e3d247.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "69e3d247204b19396307",
+    "url": "/workshop/static/css/main.f70860dc.chunk.css"
+  },
+  {
+    "revision": "27f04cf888a4aa2145234249353ff3ca",
+    "url": "/workshop/index.html"
+  }
+];
